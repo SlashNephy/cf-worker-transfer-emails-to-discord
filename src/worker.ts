5d6ab45c72a60784ad301ea46657c9f6aae6cecd
SlashNephy <spica@starry.blue>
2023-09-03 13:54:15 +0900

    publish

---
+++ src/worker.ts
@@ -0,0 +1,42 @@
+import type { ForwardableEmailMessage } from '@cloudflare/workers-types'
+
+export type Env = {
+  FORWARD_TO?: string
+  DISCORD_WEBHOOK_URL: string
+}
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  async email(message: ForwardableEmailMessage, env: Env): Promise<void> {
+    if (env.FORWARD_TO) {
+      await message.forward(env.FORWARD_TO)
+    }
+
+    await executeDiscordWebhook(env.DISCORD_WEBHOOK_URL, message)
+  },
+}
+
+async function executeDiscordWebhook(
+  url: string,
+  message: ForwardableEmailMessage
+): Promise<void> {
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username:
+        message.headers.get('From')?.replace(/^"(.+)" <(.+)>$/, '$1 <$2>') ??
+        message.from,
+      embeds: [
+        {
+          title: message.headers.get('Subject'),
+          footer: {
+            text: message.to,
+          },
+        },
+      ],
+    }),
+  })
+}
